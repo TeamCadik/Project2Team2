35e532eaee0b7b5c4f34a71a4c1ac76b71ea8367
alex.heydorn <alexkheydorn@gmail.com>
2018-09-24 18:38:24 -0400

    Adding User bean

---
+++ Icarus/src/main/java/com/revature/beans/User.java
@@ -0,0 +1,11 @@
+package com.revature.beans;
+
+public class User {
+
+	private Integer userId;
+	private String username;
+	private String password;
+	private String firstname;
+	private String lastname;
+	private Integer isDeveloper;
+}
