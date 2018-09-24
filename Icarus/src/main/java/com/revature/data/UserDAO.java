b7980dcf32f7df1f7ae64e21da9124199d82949e
Collin Lenon <clenon1812@yahoo.com>
2018-09-24 19:21:23 -0400

    Added UserDAO

---
+++ Icarus/src/main/java/com/revature/data/UserDAO.java
@@ -0,0 +1,12 @@
+package com.revature.data;
+
+import com.revature.beans.User;
+
+public interface UserDAO {
+	public int addUser(User user);
+	public User getUser(String username, String password);
+	public User getUser(User u);
+	public User getUserById(User u);
+	public void deleteUser(User user);
+	public void updateUser(User user);
+}
