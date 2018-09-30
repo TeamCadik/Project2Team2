8a50c2be493ba05a5f328b495c5b82e52829a674
DHoffmann80 <davehoffmann@live.com>
2018-09-29 20:45:25 -0400

    deleted this folder

--- AngularIcarus/src/app/build/shared/armour.ts
+++
@@ -1,9 +0,0 @@
-
-export class Armour{
-    armourId: number;
-    armourType: string;
-    defenseModifier: number;
-    stat: string;
-    statModifier: number;
-    description: string;
-}
--- AngularIcarus/src/app/build/shared/weapon.ts
+++
@@ -1,8 +0,0 @@
-
-export class Weapon{
-    weaponId: number;
-    weaponType: string;
-    attackModifier: number;
-    defenseModifier: number;
-    description: string;
-}
