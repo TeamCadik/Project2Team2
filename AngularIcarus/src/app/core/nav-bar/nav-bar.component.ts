acc23ea9ac4059e99550c5cccddd9d31b724563b
alexkheydorn <42544447+alexkheydorn@users.noreply.github.com>
2018-10-02 19:00:35 -0400

    Update nav-bar.component.ts

--- AngularIcarus/src/app/core/nav-bar/nav-bar.component.ts
+++ AngularIcarus/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/user/user.service';
-
-@Component({
+import { Router } from '@angular/router';
+ @Component({
   selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
-})
+   templateUrl: './nav-bar.component.html',
+   styleUrls: ['./nav-bar.component.css']
+ })
 export class NavBarComponent implements OnInit {
   title = "Cadik's Set Builder"
-
-  constructor(private userService: UserService) { }
-
-  ngOnInit() {
-
-
+   constructor(private userService: UserService,
+    private router: Router) { }
+   ngOnInit() {
+   }
+   isLoggedIn(): boolean {
+    return this.userService.isLoggedIn();
   }
-
-}
+   isProfilePage(): boolean {
+    if (this.router.url.includes('/profile')){
+      return true;
+    }
+    return false;
+  }
+ }
