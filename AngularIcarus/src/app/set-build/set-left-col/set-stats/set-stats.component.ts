b29baf10eff7cd16e4279991d7c4c622b95e34eb
DHoffmann80 <davehoffmann@live.com>
2018-10-04 19:32:37 -0400

    Updated the stats component to display stats base 5 per level

--- AngularIcarus/src/app/set-build/set-left-col/set-stats/set-stats.component.ts
+++ AngularIcarus/src/app/set-build/set-left-col/set-stats/set-stats.component.ts
@@ -1,24 +1,42 @@
+import { Router } from '@angular/router';
 import { StatService } from './../../../shared/stats.service';
 import { Component, OnInit } from '@angular/core';
 import { Stats } from '../../../shared/stats';
+import { Character } from '../../../profile/shared/user-profile';
+import { UserProfileService } from '../../../profile/shared/user-profile.service';
 
 @Component({
   selector: 'app-set-stats',
   templateUrl: './set-stats.component.html',
   styleUrls: ['./set-stats.component.css'],
-  providers: []
+  providers: [UserProfileService]
 })
 export class SetStatsComponent implements OnInit {
-  hitPoints = 5;
-  strength= 5;
-  dexterity= 5;
-  intel= 5;
-  defense= 5;
-  attack= 5;
+  hitpoints = 5;
+  strength  = 5;
+  dexterity  = 5;
+  intelligence = 5;
+  defense = 5;
+  attack = 5;
+  character: Character = new Character(null, null, null, null, null, null, null, null, null, null, null);
+  href = '';
 
-  constructor() {}
+  dummyText = '';
+
+  constructor(private urlRouter: Router, private userProf: UserProfileService) {}
 
   ngOnInit() {
+    this.getCharIdFromUrl();
+  }
+
+  getCharIdFromUrl() {
+    this.href = this.urlRouter.url;
+    console.log(this.href);
+    this.dummyText = this.href.slice(11);
+    console.log(this.dummyText);
+    this.character.characterId = +this.dummyText;
+    console.log(this.character.characterId);
+    this.userProf.getCharacter(this.character.characterId).subscribe(curChar => this.character = curChar);
   }
 
 }
