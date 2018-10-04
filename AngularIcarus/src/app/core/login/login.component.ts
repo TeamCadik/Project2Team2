1c16fb867a67e1a4b35e27baf72e610759348943
Collin Lenon <clenon1812@yahoo.com>
2018-10-04 18:46:41 -0400

    Added route upon logout

--- AngularIcarus/src/app/core/login/login.component.ts
+++ AngularIcarus/src/app/core/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/user/user.service';
 import { User } from '../../shared/user/user';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,9 @@ export class LoginComponent implements OnInit {
   public username: string;
   public password: string;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     this.userService.login(null, null).subscribe( user => {
@@ -32,5 +35,7 @@ export class LoginComponent implements OnInit {
     this.loggedUser = null;
     this.username = null;
     this.password = null;
+    this.router.navigate(['']);
+
   }
 }
